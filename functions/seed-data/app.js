4b76840d401abe6edad4ae5f1f352a06febd4342
JasonMrX <jiasenxu@gmail.com>
2022-02-27 14:34:10 -0800

    Create seed-data function and define messages table

---
+++ functions/seed-data/app.js
@@ -0,0 +1,23 @@
+const crypto = require("crypto");
+const aws = require("aws-sdk");
+const dynamo = new aws.DynamoDB.DocumentClient()
+const MessagesTableName = process.env.MESSAGES_TABLE
+
+exports.handler = async (event, context) => {
+    messagesCount = event["messages_count"]
+    collectionId = crypto.randomBytes(16).toString("hex"), // Unique ID for the transaction
+    promises = []
+    for (i = 0; i < messagesCount; i++) {
+        promises.push(dynamo.put({
+            TableName: MessagesTableName,
+            Item: {
+                CollectionId: collectionId,
+                Message: crypto.randomBytes(256).toString("hex")
+            }
+        }).promise())
+    }
+    await Promise.all(promises);
+    return {
+        CollectionId: collectionId
+    };
+};
