d72d395d4f39f4e7b4123f2c6df3e2b7b4b19336
JasonMrX <jiasenxu@gmail.com>
2022-02-27 18:00:35 -0800

    Create spitter function and fix sqs sendmessage policy

--- functions/splitter/app.js
+++ functions/splitter/app.js
@@ -1,17 +1,31 @@
 const aws = require("aws-sdk");
+const sqs = new aws.SQS();
 const dynamo = new aws.DynamoDB.DocumentClient()
 const MessagesTableName = process.env.MESSAGES_TABLE
-const BatchQueueName = process.env.BATCH_QUEUE
+const BatchQueueUrl = process.env.BATCH_QUEUE
 
 exports.handler = async (event, context) => {
-    collectionId = event["collection_id"];
-    result = await dynamo.query({
-        TableName: MessagesTableName,
-        KeyConditionExpression: 'CollectionId = :collection_id',
-        ExpressionAttributeValues: {
-            ':collection_id': collectionId,
-        },
-        Limit: 20,
-    }).promise()
-    return result;
+    var collectionId = event["collection_id"];
+    var batchSize = event["batch_size"];
+    var exclusiveStartKey;
+    var result;
+    do {
+        result = await dynamo.query({
+            TableName: MessagesTableName,
+            KeyConditionExpression: 'CollectionId = :collection_id',
+            ExpressionAttributeValues: {
+                ':collection_id': collectionId,
+            },
+            Limit: batchSize,
+            ExclusiveStartKey: exclusiveStartKey
+        }).promise();
+        
+        await sqs.sendMessage({
+            QueueUrl: BatchQueueUrl,
+            MessageBody: JSON.stringify({
+                message_ids: result.Items.map(item => item.MessageId)
+            })
+        }).promise();
+        exclusiveStartKey = result.LastEvaluatedKey;
+    } while (result.LastEvaluatedKey);
 };
