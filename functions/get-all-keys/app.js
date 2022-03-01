be2232717a50c21fd2dba140efce7f2ee048edbe
JasonMrX <jiasenxu@gmail.com>
2022-03-01 00:33:30 -0800

    Integrate everything with orchestrator; Replace get-all-keys with sfn sdk integration

--- functions/get-all-keys/app.js
+++
@@ -1,7 +0,0 @@
-const aws = require("aws-sdk");
-const dynamo = new aws.DynamoDB.DocumentClient()
-const keysTableName = process.env.KEYS_TABLE
-
-exports.handler = async (event, context) => {
-    return await dynamo.scan({TableName: keysTableName}).promise();
-};
